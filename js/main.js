f5ab0b3705755acc4a6cd9e4d04d0f72af7a9cfb
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2023-01-31 10:26:17 -0300

    InitI Commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+/*Mapeamento*/ 
+document.querySelector("#sobre").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollTo("#min");
+
+    console.log("go to up");
+});
+
+function scrollTo(element) {
+    document.querySelector(element).scrollIntoView({ behavior: "smooth" });
+}
+
+document.querySelector("#habilidaes").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollTo("#idades");
+
+    console.log("go to up");
+});
+
+document.querySelector("#entre").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollTo("#contato");
+
+    console.log("go to up");
+});
+
+document.querySelector("#seta").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollTo("#heard");
+
+    console.log("go to up");
+});
+
+/*Btn*/
+window.addEventListener('scroll', function () {
+    let scroll = document.querySelector('.scrolltop')
+        scroll.classList.toggle('active', window.scrollY > 200)
+})
+
+/*Pre-loand*/
+function loading() {
+    document.getElementsByClassName('load')[0].style.display = "none";
+    document.getElementsByClassName('loandig')[0].style.display = "block";
+}
